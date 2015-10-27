dd7c55a3eb96c0c6bdba50d89d8235ea6bc1df64
Yun <zcloud.zy@gmail.com>
2015-10-27 13:04:45 +0800

    增加event_type_description helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,20 @@
 module EventsHelper
+  def event_type_description(event_type)
+    case event_type
+    when "event_new"
+      "创建了任务："
+    when "event_deleted"
+      "删除了任务："
+    when "event_completed"
+      "完成了任务："
+    when "event_assignment"
+      "给 xxx 指派了任务："
+    when "event_change_assignment"
+      "把 xxx 的任务指派给 xxx："
+    when "event_modify_end_time"
+      "将任务完成时间从 xxx 修改为 xx："
+    when "event_comment"
+      "回复了任务："
+    end
+  end
 end
